337ad7b9553b172f23595a7c0d7e32a2a6a523c9
jnguyen-314 <79228845+jnguyen-314@users.noreply.github.com>
2022-04-22 16:31:48 -0400

    Add files via upload

---
+++ javascript/scriptfaq.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+
+class theGallery {};
+	
+	theGallery.images = [
+		"photo.png",
+		"photo2.png",
+		"photo3.png"
+	];
+
+
+	theGallery.description = [
+		"Your red Photo",
+		"Your yellow Photo",
+		"Your green Photo"
+	];
+
+//The functions below
+
+var page = 0;
+
+function leftGallery() {
+	if (page == 0) {
+		page = theGallery.images.length-1;
+	}
+	else {
+		page--;
+	}
+	printGallery();
+}
+
+function rightGallery() {
+	if (page == theGallery.images.length-1) {
+		page = 0;
+	}
+	else {
+		page++;
+	}
+	printGallery();
+}
+
+function printGallery() {
+	document.querySelector("p.gallery.caption").innerHTML = "&emsp;" + theGallery.description[page] + "&emsp;";//description change
+	document.querySelector("div.imagebackground").style.backgroundImage = "linear-gradient(180deg, #fff0, #fff0, #fff0, #000f)";
+	document.querySelector("div.imagebackground").style.backgroundImage += ",url('image/" + theGallery.images[page] + "')";//image change
+}
